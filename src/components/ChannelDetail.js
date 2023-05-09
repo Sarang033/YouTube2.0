4befcbdd136ea3eb1f53daac2021613e60818745
Sarang Sharma <sarangchamp2--4>
2023-05-09 08:30:01 +0530

    ytclone2

---
+++ src/components/ChannelDetail.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ChannelDetail = () => {
+  return (
+    <div>
+      channel
+    </div>
+  )
+}
+
+export default ChannelDetail
---
+++ src/components/VideoCard.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import {Link} from 'react-router-dom';
+import { Typography,Card,CardContent,CardMedia } from '@mui/material';
+import {CheckCircle} from '@mui/icons-material';
+
+import { demoThumbnailUrl,demoVideoUrl,demoVideoTitle,demoChannelUrl,demoChannelTitle } from '../utils/constants';
+
+const VideoCard = ({video:{id:{
+    videoId },snippet} }) => {
+  return (
+    <Card sx={{width:{md:'250px', xs:'100%'},boxShadow:'none',borderRadius:0 }}>
+        <Link to={videoId?`/video/${videoId}`:demoVideoUrl}>
+          <CardMedia image={snippet?.thumbnails?.high?.url}
+          alt={snippet?.title}
+          sx={{width:250,height:130}}/>
+        </Link>
+        <CardContent sx={{backgroundColor:'#1e1e1e', height:'50px'}}>
+        <Link to={videoId?`/video/${videoId}`:demoVideoUrl}>
+          <Typography variant="subtitle2" fontWeight="bold" color="#FFF">
+            {snippet?.title.slice(0,50) || demoVideoTitle.slice(0,50)}
+          </Typography>
+        </Link>
+        <Link to={snippet?.channelId?`/channel/${snippet?.channelId}`:demoChannelUrl}>
+          <Typography variant="subtitle2" fontWeight="bold" color="gray">
+            {snippet?.channelTitle || demoChannelTitle}
+            <CheckCircle sx={{fontSize:10, color:'gray',ml:'5px'}}/>
+          </Typography>
+        </Link>
+        </CardContent>
+    </Card>
+  )
+}
+
+export default VideoCard
