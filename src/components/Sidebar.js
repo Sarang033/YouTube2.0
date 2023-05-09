26f325f4f8cbd7d38ad2bab385a76b126f7406c1
Sarang Sharma <sarangchamp2--4>
2023-05-09 11:00:05 +0530

    related vids

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,30 +1,46 @@
-import React from 'react'
-import {Stack} from '@mui/material';
-import {Categories, categories} from '../utils/constants';
+import React from "react";
+import { Stack } from "@mui/material";
+import { Categories, categories } from "../utils/constants";
 
-
-const Sidebar = ({selectedCategory,setSelectedCategory}) => {
+const Sidebar = ({ selectedCategory, setSelectedCategory }) => {
   return (
-    <Stack direction="row"
-    sx={{
-        overflow:"auto",
-        height:{sx:'auto',md:'95%'},
-        flexDirection:{md:'column'}
-    }}>
-      {categories.map((category)=>(
-        <button className='category-btn'
-        onClick={()=> setSelectedCategory(category.name)}
-        style={{
-            background:category.name === selectedCategory && '#FC1503',
-            color:'white'
-        }}
-        key={category.name}>
-            <span style={{color:category.name === selectedCategory ?'white':'red', marginRight:'15px'}}>{category.icon}</span>
-            <span style={{opacity:category.name === selectedCategory? '1':'0.8'}}>{category.name}</span>
+    <Stack
+      direction="row"
+      sx={{
+        overflow: "auto",
+        height: { sx: "auto", md: "95%" },
+        flexDirection: { md: "column" },
+      }}
+    >
+      {categories.map((category) => (
+        <button
+          className="category-btn"
+          onClick={() => setSelectedCategory(category.name)}
+          style={{
+            background: category.name === selectedCategory && "#FC1503",
+            color: "white",
+          }}
+          key={category.name}
+        >
+          <span
+            style={{
+              color: category.name === selectedCategory ? "white" : "red",
+              marginRight: "15px",
+            }}
+          >
+            {category.icon}
+          </span>
+          <span
+            style={{
+              opacity: category.name === selectedCategory ? "1" : "0.8",
+            }}
+          >
+            {category.name}
+          </span>
         </button>
       ))}
     </Stack>
-  )
-}
+  );
+};
 
-export default Sidebar
+export default Sidebar;
--- src/components/VideoCard.js
+++ src/components/VideoCard.js
@@ -1,34 +1,58 @@
-import React from 'react'
-import {Link} from 'react-router-dom';
-import { Typography,Card,CardContent,CardMedia } from '@mui/material';
-import {CheckCircle} from '@mui/icons-material';
+import React from "react";
+import { Link } from "react-router-dom";
+import { Typography, Card, CardContent, CardMedia } from "@mui/material";
+import { CheckCircle } from "@mui/icons-material";
 
-import { demoThumbnailUrl,demoVideoUrl,demoVideoTitle,demoChannelUrl,demoChannelTitle } from '../utils/constants';
+import {
+  demoThumbnailUrl,
+  demoVideoUrl,
+  demoVideoTitle,
+  demoChannelUrl,
+  demoChannelTitle,
+} from "../utils/constants";
 
-const VideoCard = ({video:{id:{
-    videoId },snippet} }) => {
+const VideoCard = ({
+  video: {
+    id: { videoId },
+    snippet,
+  },
+}) => {
   return (
-    <Card sx={{width:{md:'250px', xs:'100%'},boxShadow:'none',borderRadius:0 }}>
-        <Link to={videoId?`/video/${videoId}`:demoVideoUrl}>
-          <CardMedia image={snippet?.thumbnails?.high?.url}
+    <Card
+      sx={{
+        width: { md: "250px", xs: "100%" },
+        boxShadow: "none",
+        borderRadius: 0,
+      }}
+    >
+      <Link to={videoId ? `/video/${videoId}` : demoVideoUrl}>
+        <CardMedia
+          image={snippet?.thumbnails?.high?.url}
           alt={snippet?.title}
-          sx={{width:250,height:130}}/>
-        </Link>
-        <CardContent sx={{backgroundColor:'#1e1e1e', height:'50px'}}>
-        <Link to={videoId?`/video/${videoId}`:demoVideoUrl}>
+          sx={{ width: 250, height: 130 }}
+        />
+      </Link>
+      <CardContent sx={{ backgroundColor: "#1e1e1e", height: "50px" }}>
+        <Link to={videoId ? `/video/${videoId}` : demoVideoUrl}>
           <Typography variant="subtitle2" fontWeight="bold" color="#FFF">
-            {snippet?.title.slice(0,50) || demoVideoTitle.slice(0,50)}
+            {snippet?.title.slice(0, 50) || demoVideoTitle.slice(0, 50)}
           </Typography>
         </Link>
-        <Link to={snippet?.channelId?`/channel/${snippet?.channelId}`:demoChannelUrl}>
+        <Link
+          to={
+            snippet?.channelId
+              ? `/channel/${snippet?.channelId}`
+              : demoChannelUrl
+          }
+        >
           <Typography variant="subtitle2" fontWeight="bold" color="gray">
             {snippet?.channelTitle || demoChannelTitle}
-            <CheckCircle sx={{fontSize:10, color:'gray',ml:'5px'}}/>
+            <CheckCircle sx={{ fontSize: 10, color: "gray", ml: "5px" }} />
           </Typography>
         </Link>
-        </CardContent>
+      </CardContent>
     </Card>
-  )
-}
+  );
+};
 
-export default VideoCard
+export default VideoCard;
